b2ab2eb4a48ade68ed992d5dbf00b3675d8cf1a8
yhl <love90yhl@hotmail.com>
2018-03-28 09:40:51 +0800

    加入投票的基础类

--- src/main/java/com/hx/eplate/trafficdata/query/chain/Test.java
+++ src/main/java/com/hx/eplate/trafficdata/query/chain/Test.java
@@ -6,7 +6,16 @@ package com.hx.eplate.trafficdata.query.chain;
 public class Test {
     //运行测试
     public static void main(String[] args) throws Exception{
-        //比如2人手中已存在的配型
-        String [] people1 = new String[5];
+        BallotContract.create(getCtx("sender"),new String[]{"提案1","提案2","提案3","提案4"});
+        String address001 = "voter_0x001";
+        boolean result = BallotContract.getDefault(getCtx(address001)).giveVoteRight(address001);
+        log("giveVoteRight result = "+result);
+    }
+    private static Ctx getCtx(String sender){
+        return new Ctx(new Ctx.Msg(sender));
+    }
+
+    private static void log(String msg){
+        System.out.println(msg);
     }
 }
